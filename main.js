2c3fe5c96a419799ae038bc46232ca94da133d7b
cristobal varela <cristobal.varela77@gmail.com>
2023-04-21 17:12:18 -0400

    Delete function created

--- main.js
+++ main.js
@@ -6,17 +6,42 @@ function create(event) {
   event.preventDefault();
   const tarea = readForm();
   createRow(tarea);
-  tareas.push(tarea)
+  tareas.push(tarea);
   clearForm();
   saveDataLS();
-  ;
 }
 
 function readForm() {
   const taskInput = document.getElementById("task");
   const descInput = document.getElementById("description");
 
+  const fechaTiempo = document.getElementById("time");
+
+  let fecha = new Date(fechaTiempo.value);
+
+ if(fechaTiempo.value === ""){
+    fecha = new Date()
+}
+  const anio = fecha.getFullYear();
+  const mes = fecha.getMonth() + 1;
+  const dia = fecha.getDate();
+  const hora = fecha.getHours();
+  const minutos = fecha.getMinutes();
+  const segundos = fecha.getSeconds();
+
+  const agregarCeros = (numero) => {
+    if (numero < 10) {
+      return "0" + numero;
+    } else {
+      return numero;
+    }
+  }; 
+
+  const fullFecha =  agregarCeros(dia) + "/" + agregarCeros(mes) + "/" + anio + " " + agregarCeros(hora) +":" + agregarCeros(minutos) +":"+ agregarCeros(segundos);
+ 
+
   const tarea = {
+    time: fullFecha,
     task: taskInput.value,
     desc: descInput.value,
     id: Date.now(),
@@ -30,9 +55,10 @@ function createRow(tarea) {
 
   tbody.innerHTML += `
          <tr>
+         <td>${tarea.time}</td>
             <td>${tarea.task}</td>
             <td>${tarea.desc}</td>
-            <td>
+            <td class= "buttons">
                 <button class="edit" onclick=editTask(${tarea.id})>Editar</button>
                 <button class="delete" onclick=deleteTask(${tarea.id})>Eliminar</button>
             </td>
@@ -54,15 +80,26 @@ function readFromLS() {
   tareas.forEach((el) => createRow(el));
 }
 
-
 function deleteTask(id) {
-  const index = tareas.findIndex((tarea) => tarea.id == id);
-  tareas.splice(index, 1);
-  saveDataLS();
-  tbody.innerHTML= ""
-  readFromLS();
-
+  Swal.fire({
+    title: "Estas seguro?",
+    text: "No podrás revertir los cambios",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si, borrar!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const index = tareas.findIndex((tarea) => tarea.id === id);
+      tareas.splice(index, 1);
+      saveDataLS();
+      tbody.innerHTML = "";
+      readFromLS();
+      Swal.fire("Borrado!", "La tarea ha sido borrada.", "success");
+    }
+  });
 }
-readFromLS()
 
--
+readFromLS();
+
