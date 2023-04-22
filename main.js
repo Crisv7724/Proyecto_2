dcd0bbe779bdaaa878a78b54f7da926d5b3efb7c
cristobal varela <cristobal.varela77@gmail.com>
2023-04-21 20:31:19 -0400

    The Edit button is working

--- main.js
+++ main.js
@@ -1,27 +1,43 @@
-const addButton = document.getElementById("save");
-addButton.addEventListener("click", create);
 let tareas = [];
+let editMode = false;
+let editionId = null;
+
+const addButton = document.getElementById("save");
+addButton.addEventListener("click", function (event) {
+  if (editMode === false) {
+    create(event);
+  } else {
+    refresh(event);
+  }
+});
 
 function create(event) {
   event.preventDefault();
   const tarea = readForm();
-  createRow(tarea);
-  tareas.push(tarea);
-  clearForm();
-  saveDataLS();
+  const values = Object.values(tarea);
+  const resultado = values.every((value) => value !== "");
+  if (resultado === false) {
+    Swal.fire("Ups!", "Falta completar campos.", "error");
+  } else {
+    createRow(tarea);
+    tareas.push(tarea);
+    clearForm();
+    saveDataLS();
+  }
 }
 
 function readForm() {
   const taskInput = document.getElementById("task");
   const descInput = document.getElementById("description");
 
+  // Funcion para modificar print de fecha
   const fechaTiempo = document.getElementById("time");
 
   let fecha = new Date(fechaTiempo.value);
 
- if(fechaTiempo.value === ""){
-    fecha = new Date()
-}
+  if (fechaTiempo.value === "") {
+    fecha = new Date();
+  }
   const anio = fecha.getFullYear();
   const mes = fecha.getMonth() + 1;
   const dia = fecha.getDate();
@@ -29,16 +45,27 @@ function readForm() {
   const minutos = fecha.getMinutes();
   const segundos = fecha.getSeconds();
 
+  // Funcion agregar 0 a la fecha
   const agregarCeros = (numero) => {
     if (numero < 10) {
       return "0" + numero;
     } else {
       return numero;
     }
-  }; 
-
-  const fullFecha =  agregarCeros(dia) + "/" + agregarCeros(mes) + "/" + anio + " " + agregarCeros(hora) +":" + agregarCeros(minutos) +":"+ agregarCeros(segundos);
- 
+  };
+  // Concatenar fecha final
+  const fullFecha =
+    agregarCeros(dia) +
+    "/" +
+    agregarCeros(mes) +
+    "/" +
+    anio +
+    " " +
+    agregarCeros(hora) +
+    ":" +
+    agregarCeros(minutos) +
+    ":" +
+    agregarCeros(segundos);
 
   const tarea = {
     time: fullFecha,
@@ -101,5 +128,44 @@ function deleteTask(id) {
   });
 }
 
-readFromLS();
+function editTask(id) {
+  editMode = true;
+  addButton.innerText = "Actualizar";
+  addButton.style.backgroundColor = '#edc30f'
+  editionId = id ;
+  const tarea = tareas.find((tarea) => tarea.id === id);
+  
+  const taskInput = document.getElementById("task");
+  const descInput = document.getElementById("description");
+  const fullFecha = document.getElementById("time");
+  
+  taskInput.value = tarea.task;
+  descInput.value = tarea.desc;
+  fullFecha.value = tarea.time;
+}
 
+function refresh() {
+  const valores = readForm();
+  valores.id = editionId;
+
+  const index = tareas.findIndex((tarea) => tarea.id === editionId);
+  tareas.splice(index, 1, valores);
+  saveDataLS();
+
+  const row = document.getElementById(editionId);
+  row.innerHTML = 
+  `<td>${valores.time}</td>
+   <td>${valores.task}</td>
+   <td>${valores.desc}</td>
+   <td class= "buttons">
+    <button class="edit" onclick=editTask(${valores.id})>Editar</button>
+    <button class="delete" onclick=deleteTask(${valores.id})>Eliminar</button>
+   </td`;
+
+   editMode = false;
+   editionId = null;
+   addButton.innerText = 'Guardar';
+   clearForm();
+}
+
+readFromLS();
